26db1126aa3a71bd2a1abecd17e0a530102b8083
Daria Kolisnyk <dashamel17@gmail.com>
2025-02-28 14:05:55 +0300

    Реализация основных функций

--- src/category.py
+++ src/category.py
@@ -18,6 +18,24 @@ class Category:
         """
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+    def add_product(self, product: Any) -> Any:
+        """
+        Метод для записи объекта класса Product в приватный атрибут списка товаров.
+        """
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self) -> Any:
+        products_str = ""
+        for products in self.__products:
+            products_str += f'{products.name}, {products.price} руб. Остаток: {products.quantity} шт.\n'
+        return products_str
+
+    @property
+    def in_products(self) -> Any:
+        return self.__products
--- src/product.py
+++ src/product.py
@@ -16,5 +16,33 @@ class Product:
         """
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, data: dict) -> Any:
+        products: list = []
+        for product in products:
+            if product.name == data["name"]:
+                product.quantity += data["quantity"]
+                product.price = max(product.price, data["price"])
+                return product
+        new_product = cls(**data)
+        products.append(new_product)
+        return new_product
+
+    @property
+    def price(self) -> float:
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: int) -> Any:
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            if self.__price > new_price:
+                user_input = input("Подтверждаете понижение цены yes(y)/no(n): ")
+                if user_input.lower() == "y":
+                    self.__price = new_price
+            else:
+                self.__price = new_price
